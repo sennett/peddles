fa857b42a343ea3a07ce0b617561f34278e1fa68
sennett <anthony.sennett@gmail.com>
2014-08-23 22:35:01 +0000

    removing comments form gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-    // Project configuration.
+    
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         browserify: {
@@ -14,9 +14,7 @@ module.exports = function(grunt) {
         }
     });
     
-    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-browserify');
     
-    // Default task(s).
     grunt.registerTask('default', ['browserify']);
 };
