0647f0705e3226388a13010cb8c4aa84502301d8
sennett <anthony.sennett@gmail.com>
2014-08-23 21:59:11 +0000

    creating gain view CJS module; creating app module; removing old code from index.html; hacking backbone and jquery together

---
+++ src/app.js
@@ -0,0 +1,30 @@
+// annoying little hack we have to do to get jquery and backbone to 
+// play nice with browserify
+var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
+
+var GainView = require('./controls/gain/view.js');
+var GainModel = require('./controls/gain/model.js');
+
+var context = new AudioContext, oscillator, gain;
+
+gain = context.createGain();
+gain.gain.value = 0;
+gain.connect(context.destination);
+
+var running = false;
+var toggle = function(){
+	if (running)
+		oscillator.stop();
+	else {
+		oscillator = context.createOscillator();
+		oscillator.frequency.value = 200;
+		
+		oscillator.connect(gain);
+		oscillator.start(0);
+	}
+	running = !running;
+};
+
+var gainView = new GainView({model: new GainModel() });
--- src/controls/gain/view.js
+++ src/controls/gain/view.js
@@ -1,3 +1,4 @@
+var $ = require('jquery');
 var Backbone = require('backbone');
 var _ = require("underscore");
 
