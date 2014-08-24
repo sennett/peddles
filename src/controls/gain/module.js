ec382b815933393b76e30c8d3fe8639e63c255ef
sennett <anthony.sennett@gmail.com>
2014-08-24 15:12:34 +0000

    removing default gain from gain module

--- src/controls/gain/module.js
+++ src/controls/gain/module.js
@@ -2,10 +2,11 @@ var View = require('./view.js');
 var Model = require('./model.js');
 
 module.exports = function(audioContext, sourceNode, destinationNode){
+    
     var gain = audioContext.createGain();
     sourceNode.connect(gain);
     gain.connect(destinationNode);
-    gain.gain.value = 0;
+    
     new View({model: new Model({
         gainNode: gain, 
         sourceNode: sourceNode, 
